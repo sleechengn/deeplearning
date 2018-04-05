4fb1f7ceec8da5eb5f7df6f3054f82c45ba87102
sleechengn <kernel32>
2018-04-05 09:43:03 +0800

    这是一个神经网络计算图的例子

--- src/main/java/com/jbase/demo/nn/core/Variable.java
+++ src/main/java/com/jbase/demo/nn/core/Variable.java
@@ -5,7 +5,7 @@ import org.nd4j.linalg.activations.Activation;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
 
-import static cn.redguest.jbase.ai.nn.core.Calc.*;
+import static com.jbase.demo.nn.core.Calc.*;
 
 import java.util.function.Consumer;
 
